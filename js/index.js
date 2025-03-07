37f293e864983ad90397c4bb99951c4fc497e9b6
kawser2917 <kawserahmed8492@gmail.com>
2025-03-07 14:27:58 +0600

    toggle button color change with js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,19 @@
-console.log("Hello js!! We are trying to work with js")
 const blogBtn = document.getElementById("blog-btn");
 blogBtn.addEventListener('click',function(){
     const faq =  document.getElementById("faq");
     faq.classList.remove("hidden")
+})
+
+document.getElementById("transction").addEventListener('click',function(){
+    // const donationBtn = document.getElementById('donation');
+    // donationBtn.classList.remove('bg-[#B4F461]');
+    toggleBtnColor('donation');
+    document.getElementById("transction").classList.add("bg-[#B4F461]")
+})
+
+document.getElementById("donation").addEventListener('click',function(){
+    // const transBtn = document.getElementById('transction');
+    // transBtn.classList.remove('bg-[#B4F461]');
+    toggleBtnColor('transction');
+    document.getElementById("donation").classList.add("bg-[#B4F461]")
 })
---
+++ js/utilities.js
@@ -0,0 +1,4 @@
+function toggleBtnColor(id){
+    const clickedBtn = document.getElementById(id);
+    clickedBtn.classList.remove("bg-[#B4F461]")
+}
